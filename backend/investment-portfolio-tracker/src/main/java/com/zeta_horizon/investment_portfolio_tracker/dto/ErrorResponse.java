fb2c4fe263991481fc991211c04fb818f7652d02
Darshan R M <darshanrm@zeta.tech>
2025-05-18 23:29:25 +0530

    Error and Success reponse sto

--- backend/investment-portfolio-tracker/src/main/java/com/zeta_horizon/investment_portfolio_tracker/dto/ErrorResponse.java
+++ backend/investment-portfolio-tracker/src/main/java/com/zeta_horizon/investment_portfolio_tracker/dto/ErrorResponse.java
@@ -1,19 +1,16 @@
 package com.zeta_horizon.investment_portfolio_tracker.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timestamp;
-
-    public ErrorResponse(int status, String message, LocalDateTime timestamp) {
-        this.status = status;
-        this.message = message;
-        this.timestamp = timestamp;
-    }
 }
-
