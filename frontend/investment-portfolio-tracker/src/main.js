3684875a9c06929411b3115e6129d69e98b7f894
abhijitsi03 <abhijitsi@zeta.tech>
2025-05-22 15:24:11 +0530

    fixed project structure

---
+++ frontend/investment-portfolio-tracker/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
+import 'primeicons/primeicons.css'
+
+
+
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('Button', Button);
+
+
+app.mount('#app')
